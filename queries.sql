3916a174e4f752a0cacf52565c3457f2beaae8a1
Julián Gómez <91838990+Nebiros96@users.noreply.github.com>
2022-12-23 15:24:33 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -27,23 +27,6 @@ GROUP BY
 	[WeekDay], [Hour]
 ORDER BY 
 	Reproducciones DESC;
-
--- Consulta para añadir los campos de fecha a la tabla leída del csv
-
-	SELECT [Album],
-      [Song],
-      [Fecha],
-	  DATEADD(HOUR, -5, Fecha) AS Fecha_GMT,
-	  DATEPART(YEAR, fecha) [Year],
-	  DATEPART(QUARTER, fecha) [Quarter],
-	  DATEPART(MONTH, fecha) [Month],
-	  DATEPART(DAY, fecha) [Day],
-	  DATEPART(HOUR, fecha) [Hour],
-	  FORMAT(Fecha, 'yyyy-MM') AS Year_Month,
-	  FORMAT(Fecha, 'yyyy-MM-dd') AS Year_Month_Day,
-	  DATENAME(WEEKDAY, Fecha) AS [WeekDay]
-	INTO Scrobblings_fix
-  FROM [musica_julian].[dbo].[Brenoritvrezork]; 
   
 -- Consulta para traer artistas con más apariciones en un Top N y datos adicionales 
 
