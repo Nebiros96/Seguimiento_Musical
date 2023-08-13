a7e6aaa121bfa43e38892108ac1f0eeb1210ae88
Julián Gómez <91838990+Nebiros96@users.noreply.github.com>
2023-08-13 11:34:39 -0500

    Update raw_scrobblings.sql
    DROP to csv table

--- raw_scrobblings.sql
+++ raw_scrobblings.sql
@@ -1,28 +1,30 @@
--- Se importa el csv a SQL Server con el nombre de [Brenoritvrezork]
+-- Borramos e importamos el csv a SQL Server con el nombre de [Brenoritvrezork]
+DROP TABLE Brenoritvrezork
+-- Leemos datos del csv importado
+SELECT * FROM Brenoritvrezork
 -- Consulta para añadir los campos de fecha a la tabla leída del csv
 
 -- visualizamos la tabla rapidamente
- SELECT * FROM Scrobblings_fix
+SELECT * FROM Scrobblings_fix
 ORDER BY Fecha;
 
- -- Borramos datos sin eliminar tabla
- TRUNCATE TABLE Scrobblings_fix;
+-- Borramos datos sin eliminar tabla
+TRUNCATE TABLE Scrobblings_fix;
 
- -- Insertamos nueva data
- INSERT INTO Scrobblings_fix
- SELECT 
-	[Artist],
-	[Album],
-	[Song],
-	[Fecha],
-	  DATEADD(HOUR, -5, Fecha) AS Fecha_GMT,
-	  DATEPART(YEAR, fecha) [Year],
-	  DATEPART(QUARTER, fecha) [Quarter],
-	  DATEPART(MONTH, fecha) [Month],
-	  DATEPART(DAY, fecha) [Day],
-	  DATEPART(HOUR, fecha) [Hour],
-	  FORMAT(Fecha, 'yyyy-MM') AS Year_Month,
-	  FORMAT(Fecha, 'yyyy-MM-dd') AS Year_Month_Day,
-	  DATENAME(WEEKDAY, Fecha) AS [WeekDay]
-	--INTO Scrobblings_fix
-  FROM [musica_julian].[dbo].[Brenoritvrezork]; 
+-- Insertamos nueva data
+INSERT INTO Scrobblings_fix
+SELECT 
+[Artist],
+[Album],
+[Song],
+[Fecha],
+	DATEADD(HOUR, -5, Fecha) AS Fecha_GMT,
+	DATEPART(YEAR, fecha) [Year],
+	DATEPART(QUARTER, fecha) [Quarter],
+	DATEPART(MONTH, fecha) [Month],
+	DATEPART(DAY, fecha) [Day],
+	DATEPART(HOUR, fecha) [Hour],
+	FORMAT(Fecha, 'yyyy-MM') AS Year_Month,
+	FORMAT(Fecha, 'yyyy-MM-dd') AS Year_Month_Day,
+	DATENAME(WEEKDAY, Fecha) AS [WeekDay]
+FROM [musica_julian].[dbo].[Brenoritvrezork];
